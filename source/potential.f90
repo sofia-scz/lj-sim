b9c0cdd41c178b6af26a799c9c0dee6124185c95
Sofía Anna <94293518+sofia-scz@users.noreply.github.com>
2023-11-22 13:36:28 -0300

    Add files via upload

--- source/potential.f90
+++ source/potential.f90
@@ -10,27 +10,15 @@ contains
 ! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function compute_vpress(temp, L, x) result(vpress)
     real(dp), intent(in) :: temp, L, x(npart,3)
-    integer :: i, j, p, ptot, npm, npp
+    integer :: i, j
     real(dp) :: q, d2, s2, rc2, rij(3), vpress
     vpress = zero
     rc2 = (sigma*rcut)**2.0_xp
     s2 = sigma**2.0_xp
 
-    ptot = npart*(npart-1)/2-1
-    npm = npart - 1
-    npp = npart + 1
-
-    !$omp parallel do private(p,i,j,d2,q,rij) reduction(+:vpress)
-    do p=0,ptot
-        ! get particle indices
-        i = p/npm + 1
-        j = mod(p,npm) + 1
-        if (j.lt.i) then
-            i = npp - i
-            j = npp - j
-        else 
-            j = j + 1
-        end if
+    !$omp parallel do private(i,j,d2,q,rij) schedule(static,1) reduction(+:vpress)
+    do i=1,npart-1
+    do j=i+1,npart
         ! perform computations
         rij = x(i,:) - x(j,:)
         rij = rij - L*int(2.0_dp*rij/L)
@@ -39,6 +27,7 @@ function compute_vpress(temp, L, x) result(vpress)
             q = (s2/d2)*(s2/d2)*(s2/d2)
             vpress = vpress + (2.0_dp*q*q - q)
         end if
+    end do
     end do
 
     vpress = (8.0_dp*e0*vpress + npart*temp*kB)/L**3.0_xp
@@ -50,28 +39,16 @@ function compute_vpress(temp, L, x) result(vpress)
 function compute_poten(L, x) result(erg)
         real(dp), intent(in) :: L, x(npart,3)
         real(dp) :: erg
-        integer :: i, j, p, ptot, npm, npp
+        integer :: i, j
         real(dp) :: q, d2, s2, rc2, rij(3), ercut
         erg = zero
         rc2 = (sigma*rcut)**2.0_xp
         s2 = sigma**2.0_xp
         ercut = (s2/rc2)**6.0_xp - (s2/rc2)**3.0_xp
-
-        ptot = npart*(npart-1)/2-1
-        npm = npart - 1
-        npp = npart + 1
     
-        !$omp parallel do private(p,i,j,d2,q,rij) reduction(+:erg)
-        do p=0,ptot
-            ! get particle indices
-            i = p/npm + 1
-            j = mod(p,npm) + 1
-            if (j.lt.i) then
-                i = npp - i
-                j = npp - j
-            else 
-                j = j + 1
-            end if
+        !$omp parallel do private(i,j,d2,q,rij) schedule(static,1) reduction(+:vpress)
+        do i=1,npart-1
+        do j=i+1,npart
             ! perform computations
             rij = x(i,:) - x(j,:)
             rij = rij - L*int(2.0_dp*rij/L)
@@ -80,6 +57,7 @@ function compute_poten(L, x) result(erg)
                 q = (s2/d2)*(s2/d2)*(s2/d2)
                 erg = erg + (q*q - q - ercut)
             end if
+        end do
         end do
 
         erg = 4.0_dp*e0*erg
